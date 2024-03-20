f7fc73826ddf64e6ba245d16f6deb3c25c339821
Abhinav Thatipally <145450421+thatipallyabhinav12@users.noreply.github.com>
2024-03-20 06:40:24 +0000

    useEffect

--- state-intro/src/App.jsx
+++ state-intro/src/App.jsx
@@ -1,16 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Navbar'
 
 function App() {
   const [count, setCount] = useState(10)
+  const [color, setColor] = useState(0)
+  useEffect(() => {
+    alert("Hey, welcome to react js")
+  }, [])
+
+  useEffect(() => {
+    alert("Count was changed")
+    setColor(color+1)
+  }, [count])
+
+  useEffect(() => {
+    alert("Color was changed")
+  }, [color])
 
   return (
     <>
+      <Navbar color={"red" + color}/>
       <div>The count is {count}</div>
-      <button onClick={() => {setCount(count+1)}}>Update count</button>
+      <button onClick={() => { setCount(count + 1) }}>Update count</button>
       <div>Second count {count}</div>
+
     </>
   )
 }
---
+++ state-intro/src/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = ({color}) => {
+  return (
+    <div>
+      I am Navbar of {color} color hehe
+    </div>
+  )
+}
+
+export default Navbar
